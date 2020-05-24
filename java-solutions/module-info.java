a41e589aa330ad6198ab4f1284f54e9a3dcd187c
roman <romanfml31@gmail.com>
2020-05-24 16:44:01 +0300

    add module-info.java

---
+++ java-solutions/module-info.java
@@ -0,0 +1,10 @@
+module ru.ifmo.rain.korobkov {
+    requires info.kgeorgiy.java.advanced.crawler;
+    requires info.kgeorgiy.java.advanced.hello;
+    requires info.kgeorgiy.java.advanced.arrayset;
+    requires info.kgeorgiy.java.advanced.base;
+    requires info.kgeorgiy.java.advanced.concurrent;
+    requires info.kgeorgiy.java.advanced.mapper;
+    requires info.kgeorgiy.java.advanced.student;
+    requires info.kgeorgiy.java.advanced.walk;
+}
